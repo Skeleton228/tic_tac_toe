c5e4b244babe3a56756fd1c2b3564bc6d758da94
Alexandr Karpov <sashakarpov29042007@gmail.com>
2025-10-26 18:25:12 +0300

    Добавил возможность победы

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -20,3 +20,16 @@ def game_rules(board, index, player):
     board[index - 1] = player
     next_player = 'O' if player == 'X' else 'X'
     return board, next_player
+
+
+def check_win(board):
+    win_combinations = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),
+        (0, 4, 8), (2, 4, 6)
+    ]
+
+    for a, b, c in win_combinations:
+        if board[a] == board[b] == board[c]:
+            return board[a]
+    return None
